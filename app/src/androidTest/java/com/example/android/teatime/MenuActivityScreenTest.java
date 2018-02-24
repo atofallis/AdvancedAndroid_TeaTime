d10f5510a0abbb095077cdc4c01a91819ec8c297
Aristos Tofallis <tofallis@gmail.com>
2018-02-24 16:36:24 +0000

    AdapterView test

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -16,26 +16,65 @@
 
 package com.example.android.teatime;
 
+import android.support.annotation.StringRes;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.android.teatime.model.Tea;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
- *
+ * <p>
  * This test does not utilize Idling Resources yet. If idling is set in the MenuActivity,
  * then this test will fail. See the IdlingResourcesTest for an identical test that
  * takes into account Idling Resources.
  */
 
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class MenuActivityScreenTest {
 
-    // TODO (2) Add the rule that provides functional testing of a single activity
+    @StringRes
+    final int TEA_NAME = R.string.chamomile_tea_name;
 
-    // TODO (3) Finish writing this test which will click on a gridView Tea item and verify that
-    // the OrderActivity opens up with the correct tea name displayed.
+    @Rule
+    public ActivityTestRule mRule = new ActivityTestRule(MenuActivity.class);
 
+    @Test
     public void clickGridViewItem_OpensOrderActivity() {
-
+        final String teaName = mRule.getActivity().getApplicationContext().getString(TEA_NAME);
+        onData(withTeaName(teaName))
+                .inAdapterView(withId(R.id.tea_grid_view))
+                .perform(click());
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
     }
 
+    public static Matcher<Object> withTeaName(final String name) {
+        return new BoundedMatcher<Object, Tea>(Tea.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has name " + name);
+            }
+
+            @Override
+            public boolean matchesSafely(Tea item) {
+                return name.equals(item.getTeaName());
+            }
+        };
+    }
 }
